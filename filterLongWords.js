39d3916e94f2df358f11a6881c73211874adbb05
David Pérez <devrap99@gmail.com>
2023-05-24 14:23:50 +0200

    FINISH

--- filterLongWords.js
+++ filterLongWords.js
@@ -7,6 +7,15 @@
 
 function filterLongWords(sentence, n) {
   //Solution here
+
+  //1. iNVESTIGA COMO usar el método split.
+  const array = sentence.split(" ");
+  const arrayFilter = array.filter((word) => word.length > n);
+  return arrayFilter;
+
+  console.log(array);
+
+  // 2. Usa adecuadamente el filter para filtrar el array que devuelve el split
 }
 
 console.log(filterLongWords("The quick brown fox jumps over the lazy dog", 4), [
